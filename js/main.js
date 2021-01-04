559047e5d06919c18a9e6c54bb5f00411d62deaa
v.eremenko <v.eremenko@allmonitoring.ru>
2021-01-04 22:57:28 +0300

    header PP

--- js/main.js
+++ js/main.js
@@ -1,28 +1,40 @@
-const toggleMenu = document.querySelector('.toggle-menu'); //иконка гамбургер
-const mobMenu = document.querySelector('.mobile-menu'); //mob menu
-const overlay = document.querySelector('#overlay'); //overlay
-const bodyEl = document.body;
+$(document).ready(function () {
+    //моб меню
+
+	const toggleMenu = document.querySelector('.toggle-menu'); //иконка гамбургер
+	const mobMenu = document.querySelector('.mobile-menu'); //mob menu
+	const overlay = document.querySelector('#overlay'); //overlay
+	const bodyEl = document.body;
 
 
-//прослушиваем событие клик по гамбургеру
-toggleMenu.addEventListener('click', function(){
-	
-	this.classList.toggle('active'); //  переключаем класс active  у гамбургера
-	mobMenu.classList.toggle('active');
-	overlay.classList.toggle('active');
-	bodyEl.classList.toggle('noscroll');
-});
-//прослушиваем событие клик по моб меню
-mobMenu.addEventListener('click', function(){
-	this.classList.remove('active');
-	toggleMenu.classList.remove('active');
-	overlay.classList.remove('active');
-	bodyEl.classList.remove('noscroll');
-});
-//прослушиваем событие клик по overlay
-overlay.addEventListener('click', function(){
-	this.classList.remove('active');
-	toggleMenu.classList.remove('active');
-	overlay.classList.remove('active');
-	bodyEl.classList.remove('noscroll');
-});
+	//прослушиваем событие клик по гамбургеру
+	toggleMenu.addEventListener('click', function(){
+		
+		this.classList.toggle('active'); //  переключаем класс active  у гамбургера
+		mobMenu.classList.toggle('active');
+		overlay.classList.toggle('active');
+		bodyEl.classList.toggle('noscroll');
+	});
+	//прослушиваем событие клик по моб меню
+	mobMenu.addEventListener('click', function(){
+		this.classList.remove('active');
+		toggleMenu.classList.remove('active');
+		overlay.classList.remove('active');
+		bodyEl.classList.remove('noscroll');
+	});
+	//прослушиваем событие клик по overlay
+	overlay.addEventListener('click', function(){
+		this.classList.remove('active');
+		toggleMenu.classList.remove('active');
+		overlay.classList.remove('active');
+		bodyEl.classList.remove('noscroll');
+	});
+	//-фильтрация проектов
+    let containerEl = document.querySelector('#portfolio-projects');
+
+    let mixer = mixitup(containerEl, {
+        classNames: {
+            block: ""
+        }
+    });
+})
