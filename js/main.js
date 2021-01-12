ab377bc133de43240e924a4c741477a1bf383f5b
v.eremenko <v.eremenko@allmonitoring.ru>
2021-01-13 00:12:47 +0300

    *section-contacts

--- js/main.js
+++ js/main.js
@@ -109,7 +109,7 @@ $(document).ready(function () {
 				required: 'Введите тему сообщения'
 			},
 			message: {
-				required: 'Введите текстсообщения'
+				required: 'Введите текст сообщения'
 			}
 		},
 		submitHandler: function (form) {
@@ -117,4 +117,25 @@ $(document).ready(function () {
 		}
 
 	})
+	// Функция AJAX запрса на сервер
+
+	function ajaxFormSubmit() {
+
+		let string = $(".contact-form").serialize(); // Соханяем данные введенные в форму в строку.
+
+		//Формируем ajax запрос
+		$.ajax({
+			type: "POST", // Тип запроса - POST
+			url: "php/mail.php", // Куда отправляем запрос
+			data: string, // Какие даные отправляем, в данном случае отправляем переменную string
+
+			// Функция если все прошло успешно
+			success: function (html) {
+				$(".contact-form").slideUp(800);
+				$('#answer').html(html);
+			}
+		});
+		// Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
+		return false;
+	}
 })
