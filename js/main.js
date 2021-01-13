47a30664a002d315efe3c517e2c087e30baec49b
v.eremenko <v.eremenko@allmonitoring.ru>
2021-01-13 11:55:15 +0300

    +btn-nav, +btn-up

--- js/main.js
+++ js/main.js
@@ -138,4 +138,16 @@ $(document).ready(function () {
 		// Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
 		return false;
 	}
+	// Подключение боковых точек
+	$('#page-nav').onePageNav({
+		currentClass: 'active',
+		changeHash: false,
+		scrollSpeed: 750,
+		scrollThreshold: 0.5,
+		filter: '',
+		easing: 'swing',
+		begin: function () {},
+		end: function () {},
+		scrollChange: function ($currentListItem) {}
+	});
 })
